f55534fea581ab4a0b8c9408ebd8f11bb25e0fcc
claussimar <dasunheimliche7@gmail.com>
2024-04-14 22:21:07 -0300

    cambios

--- backend/config/allowedOrigins.ts
+++ backend/config/allowedOrigins.ts
@@ -3,7 +3,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:5173",
-  "https://todo-app-test-deploy.vercel.app/",
+  "https://todo-app-test-deploy.vercel.app",
 ];
 
 export default allowedOrigins;
